f272924794eddcc413016910d761ac43dce6eaac
paulkimtest <48825944+paulkim1997@users.noreply.github.com>
2021-05-31 22:01:59 +0900

    Swagger 관련 설정 추가

--- src/main/java/com/joljak/base/config/SwaggerConfig.java
+++ src/main/java/com/joljak/base/config/SwaggerConfig.java
@@ -90,29 +90,11 @@ import java.util.List;
 public class SwaggerConfig {
 
     @Bean
-    public Docket swaggerApi() {
-        //Authentication header 처리를 위해 사용 (전역설정으로 하는경우 필요없음)
-        List global = new ArrayList();
-        global.add(new ParameterBuilder().name("Authorization").
-                description("Access Token").parameterType("header").
-                required(false).modelRef(new ModelRef("string")).build());
-
-        return new Docket(DocumentationType.SWAGGER_2).globalOperationParameters(global)
-                .apiInfo((swaggerInfo())).select()
-                .apis(RequestHandlerSelectors.basePackage("ai.lane4.api"))
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build()
-                .useDefaultResponseMessages(false);
-        //.securitySchemes(Arrays.asList(apiKey()));   Authorization설정을 전역적으로하고싶을 때 (한번 세팅해서 모든  api에 적용하고싶은경우)
-    }
-
-    private ApiKey apiKey() {
-        return new ApiKey("Bearer +jwtToken", "Authorization", "header");
-    }
-
-    private ApiInfo swaggerInfo() {
-        return new ApiInfoBuilder().title("Spring API Documentation")
-                .description("서버 API 연동 문서\n\n DEV API BASE URL : https://testapi.lane4.ai/\n" + "PROD API BASE URL : https://api.lane4.ai/")
-                .version("1").build();
+                .build();
     }
 }
